e9722e8946fee87538468f2ecfcaf6169515e592
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2019-11-04 19:19:23 +0100

    Lol, forgot to update...
    Thanks to @HimmelKreis4865

--- src/Wertzui123/BedrockClans/tasks/invitetask.php
+++ src/Wertzui123/BedrockClans/tasks/invitetask.php
@@ -4,6 +4,7 @@ namespace Wertzui123\BedrockClans\tasks;
 
 use pocketmine\Player;
 use pocketmine\scheduler\Task;
+use Wertzui123\BedrockClans\BCPlayer;
 use Wertzui123\BedrockClans\Main;
 
 class invitetask extends Task
@@ -14,7 +15,7 @@ class invitetask extends Task
     public $sender;
     public $target;
 
-    public function __construct(Main $plugin, Player $sender, Player $target,  $time)
+    public function __construct(Main $plugin, BCPlayer $sender, BCPlayer $target,  $time)
     {
         $this->plugin = $plugin;
         $this->seconds = $time;
@@ -31,15 +32,9 @@ class invitetask extends Task
     {
 
         if ($this->seconds != 0) {
-            //Sends a message to the console with how many seconds the task has been running for
-            //Checks if $this->seconds has the same value of 10
-            //Adds 1 to $this->seconds
             $this->seconds--;
         }else{
-            //Tells the console that the task is being stopped and at how many seconds
-            //Calls a function from your Main that removes the task and stops it from running
-            $this->plugin->removeTask($this->getTaskId());
-            if($this->plugin->isInvited($this->target, $this->plugin->getClan($this->sender))) {
+            if($this->sender->getClan()->isInvited($this->target)){
                 $this->plugin->expire($this->sender, $this->target);
             }
         }
