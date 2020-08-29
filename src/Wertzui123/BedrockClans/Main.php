7903462d836c54bb8bb13fabbf30814e85d9724e
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2020-08-30 01:50:35 +0200

    Fixed config updater

--- src/Wertzui123/BedrockClans/Main.php
+++ src/Wertzui123/BedrockClans/Main.php
@@ -28,7 +28,7 @@ class Main extends PluginBase
     private $withdrawCooldownsFile;
     private $players = [];
 
-    const CONFIG_VERSION = 3.1;
+    const CONFIG_VERSION = 3.2;
 
     public function onEnable(): void
     {
